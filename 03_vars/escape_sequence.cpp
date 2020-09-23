45b3cc8396d7b73ca7deab081cb0b0c7e0e2aa93
Anna Kozyreva <a.kozyreva@comforte.com>
2020-09-23 17:28:51 +0200

    add escape sequence example

---
+++ 03_vars/escape_sequence.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <climits>
+
+using std::cout;
+
+int main()
+{
+    // use for printing
+    // tab
+    cout << "Hello\tThere!\n";
+    // delete the character from the word beside him 
+    cout << "Hello\bThere!\n";
+}
