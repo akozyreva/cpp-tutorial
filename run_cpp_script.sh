83047b1eb60a9d8ee4d30dee4f47092084492221
Anna Kozyreva <a.kozyreva@comforte.com>
2020-09-24 11:31:37 +0200

    add cpplint and add nummeric_func example

--- run_cpp_script.sh
+++ run_cpp_script.sh
@@ -1,3 +1,13 @@
 #bin/bash
+clear
+echo "==============="
+echo "COMPLITING...."
 g++  ${1}
+echo "==============="
+echo "LITNTING...."
+echo "==============="
+cpplint ${1}
+echo "==============="
+echo "OUTPUT"
+echo "==============="
 ./a.out
