74d73064ede25b85f6fd9a26f2c9f8661cbc6de4
Anna Kozyreva <a.kozyreva@comforte.com>
2020-09-24 16:32:47 +0200

    add literals example

---
+++ 03_vars/literals.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // auto determines automatically the real type of variable
+    // 5 unsigned int
+    auto x = 5U;
+    std::cout << x << "\n";
+}
