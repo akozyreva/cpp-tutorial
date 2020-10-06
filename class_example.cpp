ba7428ba7db273c15ca89dc5c79bce9753d9e0dc
Anna Kozyreva <a.kozyreva@comforte.com>
2020-10-06 14:24:01 +0200

    add class example

--- class_example.cpp
+++ class_example.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class User
 {
     // all is by default private
     bool is_active = true;
 
- public:
+public:
     std::string first_name;
     std::string last_name;
     std::string status;
@@ -15,6 +16,33 @@ class User
         std::cout << is_active << "\n";
     }
 };
+void print_users(const std::vector<User> &users)
+{
+    for (User user_row : users)
+    {
+        std::cout << user_row.first_name << "\n";
+        std::cout << user_row.last_name << "\n";
+        std::cout << user_row.status << "\n";
+    }
+};
+void add_user_if_not_exists(std::vector<User> &users, User user)
+{
+    int user_is_found = 0;
+    for (User user_row : users)
+    {
+        if (user_row.first_name == user.first_name)
+        {
+            std::cout << "User is already exists"
+                      << "\n";
+            break;
+            user_is_found = 1;
+        }
+    }
+    if (user_is_found == 0)
+    {
+        users.push_back(user);
+    }
+}
 
 int main()
 {
@@ -25,4 +53,13 @@ int main()
 
     std::cout << "First name is " << me.first_name << "\n";
     me.is_user_active();
+    std::vector<User> users;
+    users.push_back(me);
+
+    User he;
+    he.first_name = "Mike";
+    he.last_name = "Smith";
+    he.status = "Platin";
+    add_user_if_not_exists(users, he);
+    print_users(users);
 }
