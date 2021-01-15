f3a24ff73781d2b2ce61ddab9f94203831ccc2fd
Anna Kozyreva <a.kozyreva@comforte.com>
2021-01-15 13:46:36 +0100

    add example, when char a doesn't print, while inputting the string

---
+++ stepik/remove_extra_spaces_from_cin_per_symbol copy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// fdkhfds   fsdhkfds ffdfs -> fdkhfds fsdhkfds ffdfs
+int main(){
+   char c;
+    while(std::cin.get(c)) 
+        if(c != 'a'){
+            std::cout << c;
+        }
+    //system("pause");
+    return 0;
+}
